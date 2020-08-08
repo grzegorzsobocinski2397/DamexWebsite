02a31a0278880e2f7ce4bd8e5bef917cd154be09
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2020-08-08 12:18:11 +0200

    Add 404 message

--- themes/damex/index.php
+++ themes/damex/index.php
@@ -4,10 +4,12 @@
     <div class="content-wrap">
         <div class="container clearfix">
             <div class="postcontent nobottommargin clearfix">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
                 the_content();
                 endwhile; else: ?>
-                <p>Sorry, no posts matched your criteria.</p>
+                <div class="no-content-message__container">
+                    <p class="no-content-message__text">Wybacz, nie znaleziono wybranego adresu.</p>
+                </div>
                 <?php endif; ?>
             </div>
         </div>
