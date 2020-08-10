500796b2aeb3e48fbdd3b559c30e6f21b4b9bd22
Grzegorz <grzegorz.sobocinski2397@gmail.com>
2020-08-10 20:51:20 +0200

    Enqueue map icons

--- themes/damex/includes/front/enqueue.php
+++ themes/damex/includes/front/enqueue.php
@@ -6,9 +6,11 @@ function ju_enqueue() {
     $ver = JU_DEV_MODE ? time() : false;
 
     wp_register_style("dl_google_fonts", "https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap" , [], $ver);
+    wp_register_style("map-icons", get_template_directory_uri() . "/assets/scss/fonts/map.icons.min.scss" , [], $ver);
     
     wp_enqueue_style( 'load-fa', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css' );
     wp_enqueue_style("dl_google_fonts");
+    wp_enqueue_style("map-icons");
     
     wp_register_script("dist-js", get_template_directory_uri() . "/assets/src/dist/main.js");
     wp_register_script("swipped-events", get_template_directory_uri() . "/assets/src/dist/swiped-events.min.js");
